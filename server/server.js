6731c87c6eace5315ddb2817a965e933f841522c
MarekStenzel <stenzelmarek@gmail.com>
2018-07-13 01:22:18 +0200

    Add queries playground file

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
-
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
